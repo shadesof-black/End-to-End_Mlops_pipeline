21f56192c4e44eb4bfc6de8faca4dece96ca8bd3
shadesof-black <rahulraj11635@gmail.com>
2026-07-09 03:27:18 +0530

    Prepare app for Render deployment

--- app.py
+++ app.py
@@ -38,5 +38,8 @@ def predict_datapoint():
 
 
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=8000)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
